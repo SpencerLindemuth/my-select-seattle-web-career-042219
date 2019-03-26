af4ecafe717e7f359653622f97ab9e44c6128991
Spencer Lindemuth <lindemut@gmail.com>
2019-03-26 23:33:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,10 +6,10 @@ def my_select(collection)
    while i < collection.length
      x = yield(collection[i])
      if x == true
-       new_collection << x
+       new_collection << collection[i]
      end
      i += 1
    end
-   collection
+   new_collection
  end
 end
