b2adc3c37b28148f873962f6c86b5f03c0b452d1
Spencer Lindemuth <lindemut@gmail.com>
2019-03-26 23:31:00 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   while i < collection.length
+     x = yield(collection[i])
+     if x == false
+       collection.delete_at(i)
+     end
+   end
+   collection
 end
